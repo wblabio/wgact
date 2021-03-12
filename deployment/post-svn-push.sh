c17e1e861b24e4ca9edfe409bbee9a1283f8bb2f
Aleksandar Vucenovic <aleksandar@wolfundbaer.ch>
2021-03-12 11:23:27 +0100

    fixed path in test deployment script

--- deployment/post-svn-push.sh
+++ deployment/post-svn-push.sh
@@ -3,4 +3,4 @@
 wp plugin deactivate woocommerce-google-adwords-conversion-tracking-tag --path='/Users/aleksandarvucenovic/dev/apps/wordpress-deployment/'
 rm ~/dev/apps/wordpress-deployment/wp-content/plugins/woocommerce-google-adwords-conversion-tracking-tag
 wp plugin install woocommerce-google-adwords-conversion-tracking-tag --path='/Users/aleksandarvucenovic/dev/apps/wordpress-deployment/'
-wp plugin activate woocommerce-google-adwords-conversion-tracking-tag --path='/Users/aleksandarvucenovic/dev/aps/wordpress-deployment/'
+wp plugin activate woocommerce-google-adwords-conversion-tracking-tag --path='/Users/aleksandarvucenovic/dev/apps/wordpress-deployment/'
