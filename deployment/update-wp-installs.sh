ee7261d845165ab3095500a2b00444176c6aa1d4
Aleksandar Vucenovic <aleksandar@wolfundbaer.ch>
2021-06-28 07:58:14 +0200

    updated wp cli update script

--- deployment/update-wp-installs.sh
+++ deployment/update-wp-installs.sh
@@ -1,10 +1,12 @@
 pushd ~/dev/apps/wordpress
+wp core update
 wp plugin update --all
 wp theme update --all
 wp language plugin update --all
 popd
 
 pushd ~/dev/apps/wordpress-deployment
+wp core update
 wp plugin update --all
 wp theme update --all
 wp language plugin update --all
