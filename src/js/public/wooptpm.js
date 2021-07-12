fd7168363f0229b35491dd66b6c63510b271eb68
Aleksandar Vucenovic <aleksandar@wolfundbaer.ch>
2021-07-12 06:20:29 +0200

    moved storeOrderIdOnServer to free version

--- src/js/public/wooptpm.js
+++ src/js/public/wooptpm.js
@@ -701,6 +701,41 @@ varExists('jQuery').then(function () {
             return "";
         }
 
+        wooptpm.storeOrderIdOnServer = function (orderId) {
+
+            // console.log('saving wooptpm_purchase_pixels_fired');
+
+            // console.log('url: ' + wooptpm_premium_only_ajax_object.ajax_url);
+            // console.log('nonce: ' + wooptpm_premium_only_ajax_object.nonce);
+
+            try {
+                // save the state in the database
+                let data = {
+                    'action'  : 'wooptpm_purchase_pixels_fired',
+                    'order_id': orderId,
+                    'nonce'   : wooptpm_premium_only_ajax_object.nonce,
+                };
+
+                jQuery.ajax(
+                    {
+                        type    : "post",
+                        dataType: "json",
+                        url     : wooptpm_premium_only_ajax_object.ajax_url,
+                        data    : data,
+                        success : function (response) {
+                            if (response.success === false) {
+                                console.log(response);
+                            }
+                        },
+                        error   : function (response) {
+                            console.log(response);
+                        }
+                    });
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
         // wooptpm['load'] = {
         //     base: true
         // };
--- src/js/public/wooptpm__premium_only.js
+++ src/js/public/wooptpm__premium_only.js
@@ -13,41 +13,7 @@ varExists('jQuery').then(function () {
 
     (function (wooptpm, $, undefined) {
 
-        wooptpm.storeOrderIdOnServer = function (orderId) {
 
-            // console.log('saving wooptpm_purchase_pixels_fired');
-
-            // console.log('url: ' + wooptpm_premium_only_ajax_object.ajax_url);
-            // console.log('nonce: ' + wooptpm_premium_only_ajax_object.nonce);
-
-
-            try {
-                // save the state in the database
-                let data = {
-                    'action'  : 'wooptpm_purchase_pixels_fired',
-                    'order_id': orderId,
-                    'nonce'   : wooptpm_premium_only_ajax_object.nonce,
-                };
-
-                jQuery.ajax(
-                    {
-                        type    : "post",
-                        dataType: "json",
-                        url     : wooptpm_premium_only_ajax_object.ajax_url,
-                        data    : data,
-                        success : function (response) {
-                            if (response.success === false) {
-                                console.log(response);
-                            }
-                        },
-                        error   : function (response) {
-                            console.log(response);
-                        }
-                    });
-            } catch (e) {
-                console.log(e);
-            }
-        }
 
     }(window.wooptpm = window.wooptpm || {}, jQuery));
 
